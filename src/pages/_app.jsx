c949b8cd9b86c8127beb7269ba98fbc49a10baf8
mboti2000 <mboti20000@gmail.com>
2023-04-17 18:35:37 +0200

    delete unnecessary code

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,10 +4,6 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import '@/styles/globals.css';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
-import { Ion } from 'cesium';
-
-Ion.defaultAccessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYjA1NzEzZi1iZDUwLTQwNWItYTI5Yy1mYjQzNjkyMWY1NmQiLCJpZCI6MTMzNzc2LCJpYXQiOjE2ODE3MzgzOTF9.WmRIZMZgIyGHZV_vIJdghfg8xD8Fj1FqeJXXfj2RCSs';
 
 export default function App({
   Component,
